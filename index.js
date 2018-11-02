6a3e3c95e199d2f3d594e75410a5cabd5759a0c0
Fabian Fabro <fabrofabian@gmail.com>
2018-11-02 13:20:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-var a, b;
+var a, b, n;
 
 function add(a, b)
 {
   return (a + b)
+}
+
+function subtract(a, b)
+{
+  return (a - b)
+}
+
+function multiply(a, b)
+{
+  return (a * b)
+}
+
+function divide(a, b)
+{
+  return (a / b)
+}
+
+function inc(n)
+{
+  n++;
+  return n;
 }
